1bbcef7553fad7c47be871908585aac006067347
Dane Dwarka <danedwarka@hotmail.com>
2022-03-10 00:17:40 -0600

    Fix bug with Memorable Quotes (Chrome CORS policy)

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,19 @@
-import MemorableQuotes from "./memorableQuotes.js";
+function MemorableQuotes() {
+  this.quote = [
+    '"The needs of the many outweigh the needs of the few." - Spock',
+    '"Live long and prosper." - Spock',
+    '"Without followers, evil cannot spread." - Spock',
+    '"After a time, you may find that having is not so pleasing a thing after all as wanting. It is not logical, but is often true." - Spock',
+    '"Insufficient facts always invite danger." - Spock',
+    '"Computers make excellent and efficient servants, but I have no wish to serve under them." - Spock',
+    '"Today is a good day to code." - Klingon Programmer',
+    `"Well it's ummm. It's green." - Scotty`
+  ]
+}
+
+MemorableQuotes.prototype.provideAQuote = function() {
+  return this.quote[Math.floor(Math.random() * (this.quote.length))];
+}
 
 const quote = new MemorableQuotes();
 
